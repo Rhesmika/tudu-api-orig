667b9171479152c76b4f3e0193ae2731feb50ea0
Rhesmika <Rhesmika@gmail.com>
2023-02-04 17:15:48 +0000

    delete teams app

--- teams/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from teams import views
-
-urlpatterns = [
-    path('teams/', views.TeamList.as_view()),
-    path('teams/<int:pk>/', views.TeamDetail.as_view()),
-
-]
